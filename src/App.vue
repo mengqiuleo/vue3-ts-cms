6b00689cd34848c6244f05136ec83fe78e59dcee
潘静怡 <1003346758@qq.com>
2022-06-23 05:24:09 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+
+
+
+
+
+
+
+    HelloWorld
+  }
+})
 </script>
 
 <style lang="less">
