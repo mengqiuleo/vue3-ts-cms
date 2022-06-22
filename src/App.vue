5a8655146cf101d5253b52affaa9b0fb622b2625
潘静怡 <1003346758@qq.com>
2022-06-23 05:38:58 +0800

    style(login): login first test

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-
-
-
-
-
-
-
     HelloWorld
   }
 })
